fc95589018336507f26540904e1c08d86e3335a5
wuchaopang <20141105049wch@mac23.local>
2015-12-02 18:32:55 +0800

    array
    定义数组，定义指针，通过指针了解数组，并且明白数组和指针的相同之处

---
+++ main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  array
+//
+//  Created by 20141105049 on 15/12/2.
+//  Copyright © 2015年 20141105049. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    int a[4];
+    int *p;
+    a[0]=10;
+    a[1]=20;
+    a[2]=30;
+    a[3]=40;
+    p=a;
+    printf("&a[0]=%x,&a[1]=%x,&a[2]=%x,&a[3]=%x,p=%x\n",a[0],a[1],a[2],a[3],p);
+    return 0;
+}
